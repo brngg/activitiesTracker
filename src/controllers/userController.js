ba9d75b55f8aafce846132266facdcfeef0eef87
unknown <chengbr3@gmail.com>
2024-07-13 00:02:30 -0400

    Able to remove/delete users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 // src/controllers/userController.js
 const User = require('../models/User');
 
+
+
 exports.createUser = async (req, res) => {
   try {
     const newUser = new User({
@@ -13,5 +15,21 @@ exports.createUser = async (req, res) => {
     res.status(201).json(savedUser);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id.trim(); // Trim any extra whitespace
+    const result = await User.deleteOne({ _id: userId });
+
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting user');
   }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/', userController.createUser);
-// Add other routes as needed
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
