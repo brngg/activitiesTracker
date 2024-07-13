dec448690db1c5bb51c0f4ed79980307a4725a54
unknown <chengbr3@gmail.com>
2024-07-13 00:33:07 -0400

    Added delete for items by name

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -5,5 +5,5 @@ const itemController = require('../controllers/itemController');
 
 router.post('/', itemController.createItem);
 // Add other item routes as needed
-
+router.delete('/:name', itemController.deleteItem);
 module.exports = router;
