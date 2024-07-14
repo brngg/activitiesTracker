16f29b01ef7b74813d6e9c30940783e894a9dd4e
unknown <chengbr3@gmail.com>
2024-07-14 00:37:22 -0400

    Finally fixed project file strucutre

--- src/routes/itemRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/itemRoutes.js
-const express = require('express');
-const router = express.Router();
-const itemController = require('../controllers/itemController');
-
-router.post('/', itemController.createItem);
-router.delete('/:name', itemController.deleteItem);
-
-module.exports = router;
